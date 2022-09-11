bbdfcffbf16fab96acadf901815791bccda5445b
Remix Workspace <>
2022-09-11 16:10:48 +0530

    First Assignment

---
+++ contracts/ERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+}
